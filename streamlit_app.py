7fc8f015661b9298cb409b60a90ef05492a048ba
austinrdu5 <59622448+austinrdu5@users.noreply.github.com>
2024-06-08 14:42:45 -0700

    Put secrets in Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,46 @@
+import os
 import streamlit as st
 import pandas as pd
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+from oauth2client.service_account import ServiceAccountCredentials
 from io import BytesIO
-import datetime
+import json
+
+# Read environment variables
+GDRIVE_TYPE = st.secrets["GDRIVE_TYPE"]
+GDRIVE_PROJECT_ID = st.secrets["GDRIVE_PROJECT_ID"]
+GDRIVE_PRIVATE_KEY_ID = st.secrets["GDRIVE_PRIVATE_KEY_ID"]
+GDRIVE_PRIVATE_KEY = st.secrets["GDRIVE_PRIVATE_KEY"]
+GDRIVE_CLIENT_EMAIL = st.secrets["GDRIVE_CLIENT_EMAIL"]
+GDRIVE_CLIENT_ID = st.secrets["GDRIVE_CLIENT_ID"]
+GDRIVE_AUTH_URI = st.secrets["GDRIVE_AUTH_URI"]
+GDRIVE_TOKEN_URI = st.secrets["GDRIVE_TOKEN_URI"]
+GDRIVE_AUTH_PROVIDER_X509_CERT_URL = st.secrets["GDRIVE_AUTH_PROVIDER_X509_CERT_URL"]
+GDRIVE_CLIENT_X509_CERT_URL = st.secrets["GDRIVE_CLIENT_X509_CERT_URL"]
+
+# Create a dictionary for the service account credentials
+credentials_dict = {
+    "type": GDRIVE_TYPE,
+    "project_id": GDRIVE_PROJECT_ID,
+    "private_key_id": GDRIVE_PRIVATE_KEY_ID,
+    "private_key": GDRIVE_PRIVATE_KEY,
+    "client_email": GDRIVE_CLIENT_EMAIL,
+    "client_id": GDRIVE_CLIENT_ID,
+    "auth_uri": GDRIVE_AUTH_URI,
+    "token_uri": GDRIVE_TOKEN_URI,
+    "auth_provider_x509_cert_url": GDRIVE_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": GDRIVE_CLIENT_X509_CERT_URL
+}
+
+# Save the credentials dictionary as a JSON string and load it into ServiceAccountCredentials
+credentials_json = json.dumps(credentials_dict)
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(json.loads(credentials_json))
 
 # Authenticate and initialize PyDrive
-def authenticate_drive():
-    gauth = GoogleAuth()
-    gauth.LocalWebserverAuth()
-    drive = GoogleDrive(gauth)
-    return drive
-
-drive = authenticate_drive()
+gauth = GoogleAuth()
+gauth.credentials = credentials
+drive = GoogleDrive(gauth)
 
 st.title("Weekly Customer Email Processor")
 
@@ -26,46 +54,8 @@ if uploaded_file is not None:
     st.write(df_new)
 
     # Get the current date and generate the filename
-    current_date = datetime.datetime.now().strftime('%Y-%m-%d')
-    filename = f'customer_emails_{current_date}.csv'
-
-    # Save the uploaded file to Google Drive
-    file_drive = drive.CreateFile({'title': filename})
-    file_drive.SetContentFile(uploaded_file.name)
-    file_drive.Upload()
-    st.write(f'File uploaded to Google Drive as {filename}')
-
-    # List files in Google Drive and filter by date
-    file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
-    date_threshold = datetime.datetime.now() - datetime.timedelta(weeks=2)
-    recent_files = [
-        file for file in file_list
-        if datetime.datetime.strptime(file['title'].split('_')[-1].replace('.csv', ''), '%Y-%m-%d') >= date_threshold
-    ]
-
-    # Collect emails from recent files
-    recent_emails = set()
-    for file in recent_files:
-        file_drive = drive.CreateFile({'id': file['id']})
-        file_drive.GetContentFile(file['title'])
-        df = pd.read_csv(file['title'])
-        recent_emails.update(df['email'])
-
-    # Filter out recent emails from the new dataframe
-    df_filtered = df_new[~df_new['email'].isin(recent_emails)]
-
-    # Display filtered dataframe
-    st.write("Filtered DataFrame:")
-    st.write(df_filtered)
-
-    # Download the filtered dataframe
-    def to_csv(df):
-        output = BytesIO()
-        df.to_csv(output, index=False)
-        processed_data = output.getvalue()
-        return processed_data
-
-    st.download_button(label="Download filtered data as CSV",
-                       data=to_csv(df_filtered),
-                       file_name='filtered_customer_emails.csv',
-                       mime='text/csv')
+    current_date = pd.Timestamp.now().strftime('%Y-%m-%d')
+    file_name = f"emails_{current_date}.csv"
+    
+    # Save the DataFrame to a CSV file
+    df_new.to_csv(file
