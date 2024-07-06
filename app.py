45e54b473a7cea38ce435d89f3295ae55cf296f5
austinrdu5 <austinrdu5@gmail.com>
2024-07-05 17:01:00 -0700

    made funcs.py to store helper functions

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 import streamlit as st
-import pandas as pd
-import os
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaIoBaseUpload
-from io import BytesIO
+from funcs import *
 
 CSV_FOLDER_ID = "1pS27r6Hpb_a17kmQPURIRNfS2RYUnZc5"
 
@@ -22,100 +17,38 @@ service_account_info = {
     "client_x509_cert_url": st.secrets["GDRIVE_CLIENT_X509_CERT_URL"],
 }
 
-# Function to authenticate with Google Drive
-def authenticate(service_account_info=service_account_info):
-    try:
-        creds = service_account.Credentials.from_service_account_info(
-            service_account_info, scopes=["https://www.googleapis.com/auth/drive"]
-        )
-        service = build('drive', 'v3', credentials=creds)
-        return service
-    except Exception as e:
-        st.error(f"Error authenticating with Google Drive: {e}")
-        st.stop()
+# -- Begin Streamlit App --
 
-# Function to list alphabetically last 3 files in the specified folder
-def get_csvs(service, folder_id=CSV_FOLDER_ID):
-    try:
-        results = service.files().list(
-            q=f"'{folder_id}' in parents",
-            orderBy='name',
-            fields='nextPageToken, files(id, name)'
-        ).execute()
-        items = results.get('files', [])
-        return items[:-4:-1]
-    except Exception as e:
-        st.error(f"Error accessing files: {e}")
-        st.stop()
-
-# Function to read CSVs by ID
-def read_csv(service, file_id):
-    try:
-        request = service.files().get_media(fileId=file_id)
-        response_content = request.execute()
-        return pd.read_csv(BytesIO(response_content))
-    except Exception as e:
-        st.error(f"Error reading file: {e}")
-        st.stop()
-
-# Function to process most recent CSVs
-def process_csvs(service, csv_list):
-    try:
-        # Read the most recent CSVs
-        dfs = [read_csv(service, item['id']) for item in csv_list]
-        df = pd.concat(dfs)
-
-        # Process the data
-        df = df.groupby('email').agg(
-            student_name=('student name', 'first'),
-            class_name=('class name', 'first'),
-            primary_parent=('primary parent', 'first')
-        ).reset_index()
-
-        # Download the CSV
-        download_csv(df, "processed_data.csv")
-    except Exception as e:
-        st.error(f"Error processing CSVs: {e}")
-        st.stop()
-
-# Function to download CSV to user's local machine
-def download_csv(df, file_name):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="{file_name}">Download CSV File</a>'
-    st.markdown(href, unsafe_allow_html=True)
-
-
-# Begin Streamlit App
 st.title("SDCV Poor HW Emailer")
 st.write("This app processes a CSV file of student information and sends emails to parents who have not completed homework.")
 
-# Authenticate with Google Drive
+# Authenticate
 service = authenticate(service_account_info)
 
-# List most recent files in the folder
-try: 
-    csv_list = get_csvs()
+# Get most recent files in the folder
+csv_list = get_csvs(service, 3, CSV_FOLDER_ID)
 
-    with st.container():
-        st.header("Most recent files in folder (alphabetically):")
+# Display most recent files
+with st.container():
+    st.header("Most recent files in folder (alphabetically):")
 
-        if not csv_list:
-            st.write('No files found.')
-        else:
-            for item in csv_list:
-                st.write(f"- {item['name']}")  # Display file names
-
-except Exception as e:
-    st.error(f"Error accessing files: {e}")
+    if not csv_list:
+        st.write('No files found.')
+    else:
+        for item in csv_list:
+            st.write(f"- {item['name']}") 
 
 st.write("To add a new file, please upload a CSV into [this Google Drive folder](https://drive.google.com/drive/folders/1pS27r6Hpb_a17kmQPURIRNfS2RYUnZc5). \
          Please note that the app uses the file names to sort by newest, so please following the naming conventions within the folder (poor_hw_reportYYYY-MM-DD....csv).")
 
-# Process and Download CSVs
+# Button to process and download CSVs
 if st.button("Get This Week's Sequence CSVs"):
     with st.spinner("Processing reports..."):
-        pass
+        
+        process_csvs(service, csv_list)
+        
+
+        
 
 # 2. File Upload and Processing
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
---
+++ funcs.py
@@ -0,0 +1,118 @@
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+import streamlit as st
+import pandas as pd
+from io import BytesIO
+import base64
+import datetime
+from googleapiclient.http import MediaIoBaseUpload
+
+
+# Function to authenticate with Google Drive
+def authenticate(service_account_info):
+    try:
+        creds = service_account.Credentials.from_service_account_info(
+            service_account_info, scopes=["https://www.googleapis.com/auth/drive"]
+        )
+        service = build('drive', 'v3', credentials=creds)
+        return service
+    except Exception as e:
+        st.error(f"Error authenticating with Google Drive: {e}")
+        st.stop()
+
+# Function to list alphabetically last 3 files in the specified folder
+# Returns a list of dictionaries with file ID and name
+def get_csvs(service, n, folder_id):
+    try:
+        results = service.files().list(
+            q=f"'{folder_id}' in parents",
+            orderBy='name',
+            fields='nextPageToken, files(id, name)'
+        ).execute()
+        items = results.get('files', [])
+        return items[:-(n+1):-1]
+    except Exception as e:
+        st.error(f"Error accessing files: {e}")
+        st.stop()
+
+# Helper function to read CSVs by ID
+def read_csv(service, file_id):
+    try:
+        request = service.files().get_media(fileId=file_id)
+        response_content = request.execute()
+        return pd.read_csv(BytesIO(response_content))
+    except Exception as e:
+        st.error(f"Error reading file: {e}")
+        st.stop()
+
+# Function that takes a pandas series and returns a string with Oxford comma
+def _oxford_comma(series):
+    # remove duplicates with set
+    seen = set()
+    str_list = [x for x in series if not (x in seen or seen.add(x))]
+
+    if len(str_list) == 1:
+        return str_list[0]
+    elif len(str_list) == 2:
+        return str_list[0] + ' and ' + str_list[1]
+    else:
+        return ', '.join(str_list[:-1]) + ', and ' + str_list[-1]
+
+# Helper function to combine rows with the same students and classes
+# Assumes df only has students of the same class level (1, 2, or 3)
+def _combine_siblings_and_classes(df):
+
+    combined = df.groupby('email').agg({'student name': _oxford_comma,
+                                        'class name': _oxford_comma,
+                                        'primary parent': 'first'}).reset_index()
+
+    return combined
+
+# Function to process most recent CSVs, returns list of DataFrames
+def process_csvs(service, csv_list):
+    current = read_csv(service, csv_list[0]['id'])
+    previous1 = read_csv(service, csv_list[1]['id'])
+    previous2 = read_csv(service, csv_list[2]['id'])
+
+    try:
+        # TODO: add conditional check here
+        previous = pd.concat([previous1, previous2])
+
+        # remove rows in current_df that occur in previous_df
+        merged_df = current.merge(previous, how='left', indicator=True)
+        to_email = merged_df[merged_df['_merge'] == 'left_only'].drop(columns='_merge')
+
+        # rename email column to 'email' (Front requires this)
+        to_email = to_email.rename(columns={'primary parent email': 'email'})
+
+        # in student name, only keep first name
+        to_email['student name'] = to_email['student name'].str.split().str[0]
+
+        # in parent name, change to title case
+        to_email['primary parent'] = to_email['primary parent'].str.title()
+
+        # only keep columns email, student name, and class name
+        to_email = to_email[['email', 'student name', 'class name', 'primary parent']]
+
+        # split into lower, middle, and upper class levels
+        lower = to_email[to_email['class level'] == 1].drop(columns=['class level'])
+        middle = to_email[to_email['class level'] == 2].drop(columns=['class level'])
+        upper = to_email[to_email['class level'] == 3].drop(columns=['class level'])
+
+        # combine siblings and classes
+        lower = _combine_siblings_and_classes(lower)
+        middle = _combine_siblings_and_classes(middle)
+        upper = _combine_siblings_and_classes(upper)
+
+        return [lower, middle, upper]   
+
+    except Exception as e:
+        st.error(f"Error while processing CSVs: {e}")
+        st.stop()
+
+# Function to download CSV to user's local machine
+def download_csv(df, file_name):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="{file_name}">Download CSV File</a>'
+    st.markdown(href, unsafe_allow_html=True)
